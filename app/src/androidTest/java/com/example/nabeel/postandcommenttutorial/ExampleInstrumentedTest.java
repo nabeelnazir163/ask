e507bb96cc67b4b460c120201f57bed27ef3889b
Nabeel <nabeelnazir163@yahoo.com>
2018-01-22 13:09:40 +0500

    22/1/18

--- app/src/androidTest/java/com/example/nabeel/postandcommenttutorial/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nabeel/postandcommenttutorial/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.nabeel.postandcommenttutorial;
+package com.zillion.nabeel.postandcommenttutorial;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -15,12 +15,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class zillionInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.nabeel.postandcommenttutorial", appContext.getPackageName());
+        assertEquals("com.zillion.nabeel.postandcommenttutorial", appContext.getPackageName());
     }
 }
